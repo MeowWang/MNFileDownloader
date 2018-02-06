9118731114ecabb3118c0f823a459095aed6918a
王雪利 <Monalisa@monawang-mc0.tencent.com>
2018-02-06 16:31:11 +0800

    create proj

---
+++ MNFileDownloader/MNFileDownloader.h
@@ -0,0 +1,13 @@
+//
+//  MNFileDownloader.h
+//  MNFileDownloader
+//
+//  Created by Monalisa on 2018/2/6.
+//  Copyright © 2018年 Monalisa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MNFileDownloader : NSObject
+
+@end
---
+++ MNFileDownloader/MNFileDownloader.m
@@ -0,0 +1,17 @@
+//
+//  MNFileDownloader.m
+//  MNFileDownloader
+//
+//  Created by Monalisa on 2018/2/6.
+//  Copyright © 2018年 Monalisa. All rights reserved.
+//
+
+#import "MNFileDownloader.h"
+
+@implementation MNFileDownloader
+
++(instancetype)shareInstance
+{
+    
+}
+@end
